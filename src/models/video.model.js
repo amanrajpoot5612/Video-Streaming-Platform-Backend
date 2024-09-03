52d1b4e75e20aaf73b1287da75e84b1321496b53
Aman Rajpoot <amanrajpoot5612@gmail.com>
2024-09-03 15:46:19 +0530

    added models

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, {Schema, schema} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,    //cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String,    //cloudinary url
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,   //cloudinary url
+        required: true
+    },
+    views: {
+        type: Number,   //cloudinary url
+        required: true,
+        default: 0
+    },
+    isPublished: {
+        type: Number,   //cloudinary url
+        required: true,
+        default: 0
+    },
+    owner: {
+        type:Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {
+    timestamps: true
+})
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+
+export const Video = mongoose.model("Video", videoSchema)
