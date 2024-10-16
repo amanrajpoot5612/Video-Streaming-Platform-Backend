611e7f520af29de22f0fcb68bd6014958d373f66
Aman Rajpoot <amanrajpoot5612@gmail.com>
2024-10-17 00:12:04 +0530

    Refactor cloudinary.js to remove console.log statements and add error handling

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
 import fs from 'fs'
 
 
@@ -8,7 +9,7 @@ import fs from 'fs'
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDIANRY_API_SECRET // Click 'View API Keys' above to copy your API secret
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
 
     const uploadOnCloudinary = async (localFilePath) => {
@@ -16,10 +17,12 @@ import fs from 'fs'
             if (!localFilePath) return null;
             //upload file on cloudinary
             const response = await cloudinary.uploader.upload(localFilePath, {
-                resource_type: "auto"
+                resource_type: "auto"    
             })
+        // console.log(response);
+            fs.unlinkSync(localFilePath);
             // file uploaded successfully
-            console.log("file is uploaded on cloudinary", response.url);
+            // console.log("file is uploaded on cloudinary", response.url);
             return response;
         } catch (error) {
             fs.unlinkSync(localFilePath)    // remove the file from server
