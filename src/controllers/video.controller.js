66b91b096bf865642bfb7a7eeedfdbbb21f2ba9f
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-17 00:19:46 +0530

    Refactor video upload logic to improve variable naming and ensure proper validation; upload files to Cloudinary before creating video entry

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,6 +2,7 @@ import { Video } from "../models/video.model.js";
 import apiError from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
 
 
 
@@ -9,11 +10,11 @@ const uploadVideo = asyncHandler(async (req , res) => {
     const {title, description, duration} = req.body;
     console.log("video uploaded started");
     
-    const videoFile = req.files?.videoFile?.[0]?.path;
-    const thumbnail = req.files?.thumbnail?.[0]?.path;
+    const videoFileLocal = req.files?.videoFile?.[0]?.path;
+    const thumbnailLocal = req.files?.thumbnail?.[0]?.path;
 
     //validation
-    if(!title || !description || !videoFile || !thumbnail){
+    if(!title || !description || !videoFileLocal || !thumbnailLocal){
         throw new apiError(400 , "All fields are required", [
             "title",
             "description",
@@ -23,12 +24,25 @@ const uploadVideo = asyncHandler(async (req , res) => {
         ])
     }
 
+    const thumbnail = await uploadOnCloudinary(thumbnailLocal)
+    const videoFile = await uploadOnCloudinary(videoFileLocal)
+    console.log("Uploaded on cloudinary");
+    
+
+    if(!videoFile){
+        throw new apiError(400, "Video file is required ( not uploaded on cloudinary)")
+    }
+
+    if(!thumbnail){
+            throw new apiError(400, "Thumbnail file is required ( not uploaded on cloudinary)")
+        }
+
     const newVideo = await Video.create({
         title,
         description,
         duration,
-        videoFile,
-        thumbnail,
+        videoFile: videoFile.secure_url,
+        thumbnail: thumbnail.secure_url,
         owner: req.user._id
     });
 
