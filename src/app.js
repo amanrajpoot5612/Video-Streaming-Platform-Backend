c8efec93a3698cdae16bf998ba9965642d1df978
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-17 15:42:43 +0530

    Refactor app.js to improve code organization and maintainability

--- src/app.js
+++ src/app.js
@@ -7,17 +7,41 @@ import videoRouter from './routes/video.routes.js'
 
 const app = express();
 dotenv.config();
+
+// proxy
+app.set("trust proxy", 1);
+
+// cors
 app.use(cors({                                  //Initialize express
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
+
+//json parser
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
+
 //Standardisation
 app.use(express.static("public"))
 app.use(cookieParser());
 
+
+
+// routes
+import userRouter from './routes/user.routes.js'
+
+// // routes declaration
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/videos", videoRouter)
+// app.use("/api/v1/admin", adminRouter)
+// app.use("/api/v1/product", productRouter)
+// app.use("/api/v1/skills", skillsRouter)
+// app.use("/api/v1/users", userRouter)
+
+
+// error handlers
 app.use((err, req, res, next) => {
     if (err instanceof apiError) {
         return res.status(err.statusCode).json({
@@ -35,18 +59,6 @@ app.use((err, req, res, next) => {
         // stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
     });
 });
-
-
-// routes
-import userRouter from './routes/user.routes.js'
-
-// // routes declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/videos", videoRouter)
-// app.use("/api/v1/admin", adminRouter)
-// app.use("/api/v1/product", productRouter)
-// app.use("/api/v1/skills", skillsRouter)
-// app.use("/api/v1/users", userRouter)
 
 
 export default app;
